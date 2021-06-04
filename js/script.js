0f0575d20df460d325ad250d82c0680c38839ce8
liderfalera <lider.falera@usil.pe>
2021-06-04 12:45:55 -0500

    COMPLETED AND POLISHED functionalities and design

--- js/script.js
+++ js/script.js
@@ -1,154 +1,254 @@
-const celeste = document.getElementById("celeste");
-const violeta = document.getElementById("violeta");
-const naranja = document.getElementById("naranja");
-const verde = document.getElementById("verde");
-const btnEmpezar = document.getElementById("btnEmpezar");
+//Colors
+const red = document.getElementById("red");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");
 
-const RANDOMUNO_CUATRO = (n) => Math.floor(Math.random() * 4);
-const ULTIMO_NIVEL = 10;
+//Input Area
+const inputArea = document.getElementById("input-level");
+
+//Start Area
+const startArea = document.getElementById("startArea");
+const playBtn = document.getElementById("playBtn");
+
+//Current level Area
+const currentLevel = document.getElementById("current-level");
+
+//CONSTS==================================================
+const DEFAULT_LEVEL = 5;
+const RANDOM_UNO_A_CUATRO = () => Math.floor(Math.random() * 4);
 
 class Juego {
   constructor() {
     this.inicializar = this.inicializar.bind(this);
     this.inicializar();
     this.generarSecuencia();
-    setTimeout(this.siguienteNivel(), 500);
+    setTimeout(this.siguienteNivel, 600);
   }
 
-  inicializar() {
-    this.siguienteNivel = this.siguienteNivel.bind(this);
+  //What the game do when starts
+  inicializar(loose) {
+
     this.elegirColor = this.elegirColor.bind(this);
-    this.toggleBtnEmpezar();
+    this.siguienteNivel = this.siguienteNivel.bind(this);
+
+    //catching the amount of levels chose
+    this.nivelDeseado = parseInt(document.getElementById("set-level").value);
+
+    //SETTING UI IF LOOSE OR WIN
+    if (loose) {
+      this.settingTryAgain()
+    }else{
+      this.settingNuevoJuego()
+    }
+    ////////////////////////////////
+
+ 
     this.nivel = 1;
     this.colores = {
-      celeste,
-      violeta,
-      naranja,
-      verde,
+      red,
+      green,
+      blue,
+      yellow,
     };
   }
 
-  //VERIFICA Y OCULTA/MUESTRA EL BOTON EMPEZAR JUEGA
-  toggleBtnEmpezar() {
-    if (btnEmpezar.classList.contains("hide")) {
-      btnEmpezar.classList.remove("hide");
+  
+  settingNuevoJuego() {
+
+    if(startArea.classList.contains('hide')){
+      startArea.classList.remove("hide");
+      inputArea.classList.remove("disabled");
+      currentLevel.classList.remove("show");
+      document.getElementById('set-level').value = ''  
+      playBtn.innerHTML = "PLAY<br/>GAME"
+    }else{
+      
+    startArea.classList.add("hide");
+    inputArea.classList.add("disabled");
+    currentLevel.classList.add("show");
+    playBtn.innerHTML = "PLAY<br/>GAME"
+ 
+    }
+
+  }
+
+  settingTryAgain() {
+    startArea.classList.remove("hide");
+    inputArea.classList.remove("disabled");
+    currentLevel.classList.remove("show");
+
+    playBtn.innerHTML = "TRY<br/>AGAIN"
+
+  }
+
+  //Generating the random secuence of  numbers for the levels
+  generarSecuencia() {
+    if (this.nivelDeseado > 0) {
+      this.secuencia = new Array(this.nivelDeseado)
+        .fill(0)
+        .map(RANDOM_UNO_A_CUATRO);
     } else {
-      btnEmpezar.classList.add("hide");
+      this.secuencia = new Array(DEFAULT_LEVEL)
+        .fill(0)
+        .map(RANDOM_UNO_A_CUATRO);
     }
   }
-  ///////////////////////////////////////////////////
 
-  //GENERAMOS LA SECUENCIA CON DIEZ NUMEROS ALEATORIOS DEL 1 AL 4
-  generarSecuencia() {
-    this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(RANDOMUNO_CUATRO);
-  }
 
-  //METODO AL PASAR DE NIVEL
   siguienteNivel() {
-    this.subnivel = 0;
+
+    this.aciertos = 0;
+    this.eliminarEventosClick();
     this.iluminarSecuencia();
-    this.agregarEventoClick();
+    //this.agregarEventosClick();
+    this.actualizarProgreso();
   }
 
-  //TRANSFORMANDO LOS NUMEROS DEL 1 AL 4 => A celeste,violeta,naranja y verde y VICEVERSA
+  /*UPDATING THE PROGRESS*/
+  actualizarProgreso() {
+    const nivelActual = this.nivel;
+    const nivelFinal = this.nivelDeseado;
+    if (nivelFinal) {
+      document.getElementById("progress-level").innerHTML =
+        nivelActual + " / " + nivelFinal;
+    } else {
+      document.getElementById("progress-level").innerHTML =
+        nivelActual + " / " + DEFAULT_LEVEL;
+    }
+  }
+
+  ////*Number to color*/
   transformarNumeroAColor(numero) {
     switch (numero) {
       case 0:
-        return "celeste";
+        return "red";
       case 1:
-        return "violeta";
+        return "green";
       case 2:
-        return "naranja";
+        return "blue";
       case 3:
-        return "verde";
+        return "yellow";
     }
   }
-
   transformarColorANumero(color) {
     switch (color) {
-      case "celeste":
+      case "red":
         return 0;
-      case "violeta":
+      case "green":
         return 1;
-      case "naranja":
+      case "blue":
         return 2;
-      case "verde":
+      case "yellow":
         return 3;
     }
   }
-  //////////////////////////////////////////////////////////////////
+  //===========================
 
-  //METODO QUE ILUMINA LA SECUENCIA DE COLORES SEGUN EL NIVEL
   iluminarSecuencia() {
     for (let i = 0; i < this.nivel; i++) {
       const color = this.transformarNumeroAColor(this.secuencia[i]);
-      setTimeout(() => this.iluminarColor(color), 1000 * i);
+      setTimeout(() => this.iluminarColor(color), 640 * i)
+      if(i === (this.nivel - 1)){
+        setTimeout(()=>this.agregarEventosClick(),((640*i)+300))
+      }
     }
+    
+    
   }
 
-  //METODOS PARA ILUMINAR Y LUEGO APAGAR
   iluminarColor(color) {
     this.colores[color].classList.add("light");
-    setTimeout(() => this.apagarColor(color), 350);
+    setTimeout(() => this.apagarColor(color), 300);
   }
 
   apagarColor(color) {
     this.colores[color].classList.remove("light");
   }
-  //////////////////////////////////////////////////
 
-  //CAPTURAMOS EL CLICK DEL USUARIO Y
-  agregarEventoClick() {
-    this.colores.celeste.addEventListener("click", this.elegirColor);
-    this.colores.violeta.addEventListener("click", this.elegirColor);
-    this.colores.naranja.addEventListener("click", this.elegirColor);
-    this.colores.verde.addEventListener("click", this.elegirColor);
+  agregarEventosClick() {
+    
+    this.colores.red.classList.add('clickable')
+    this.colores.green.classList.add('clickable')
+    this.colores.blue.classList.add('clickable')
+    this.colores.yellow.classList.add('clickable')    
+
+    this.colores.red.addEventListener("click", this.elegirColor);
+    this.colores.green.addEventListener("click", this.elegirColor);
+    this.colores.blue.addEventListener("click", this.elegirColor);
+    this.colores.yellow.addEventListener("click", this.elegirColor);
   }
   eliminarEventosClick() {
-    this.colores.celeste.removeEventListener("click", this.elegirColor);
-    this.colores.violeta.removeEventListener("click", this.elegirColor);
-    this.colores.naranja.removeEventListener("click", this.elegirColor);
-    this.colores.verde.removeEventListener("click", this.elegirColor);
+
+    this.colores.red.classList.remove('clickable')
+    this.colores.green.classList.remove('clickable')
+    this.colores.blue.classList.remove('clickable')
+    this.colores.yellow.classList.remove('clickable')  
+
+    this.colores.red.removeEventListener("click", this.elegirColor);
+    this.colores.green.removeEventListener("click", this.elegirColor);
+    this.colores.blue.removeEventListener("click", this.elegirColor);
+    this.colores.yellow.removeEventListener("click", this.elegirColor);
   }
 
   elegirColor(ev) {
-    const nombreColor = ev.target.dataset.color;
-    const numeroColor = this.transformarColorANumero(nombreColor);
-    this.iluminarColor(nombreColor);
-
-    //SI EL USUARIO TOCA EL BOTON CORRECTO
-    if (numeroColor === this.secuencia[this.subnivel]) {
-      this.subnivel++;
-      if (this.subnivel === this.nivel) {
+  
+    const colorPickedName = ev.target.dataset.color;
+    const colorPickedNumber = this.transformarColorANumero(colorPickedName);
+    this.iluminarColor(colorPickedName);
+    if (colorPickedNumber === this.secuencia[this.aciertos]) {
+      this.aciertos++;
+      if (this.aciertos === this.nivel) {
         this.nivel++;
         this.eliminarEventosClick();
-        if (this.nivel === ULTIMO_NIVEL + 1) {
-          //GANASTE EL JUEGO
+
+        if (this.nivel === (this.nivelDeseado + 1)) {
+          //GANASTE
+          document.getElementById('progress-level').innerHTML = ""
           this.ganaste();
-        } else {
-          setTimeout(this.siguienteNivel, 1500);
+
+        } else if(this.nivel === (DEFAULT_LEVEL+1)){
+          document.getElementById('progress-level').innerHTML = ""
+         this.ganaste() 
+        }else {
+          if(this.nivel === 1){
+          }else{
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Next Level 👏',
+              showConfirmButton: false,
+              timer: 720
+            }).then(()=>{
+              
+              setTimeout(this.siguienteNivel, 600);
+            })
+          }
+          
         }
       }
-    }
-    //SI EL BOTON ES INCORRECTO
-    else {
+    } else {
       //PERDISTE
+      document.getElementById('progress-level').innerHTML = ""
       this.perdiste();
     }
   }
 
   ganaste() {
-    swal("SIMON DICE", "Ganaste!", "success").then(this.inicializar);
-  }
-
-  perdiste() {
-    swal("SIMON DICE", "Perdiste! :(", "error").then(() => {
-      this.eliminarEventosClick();
+    swal("Winner!", "Simon celebrates it!😊", "success").then(() => {
+      
       this.inicializar();
+    });
+  }
+  perdiste() {
+    swal("You lose!", "Simon is Sad!😥", "error").then(() => {
+      this.eliminarEventosClick();
+      
+      this.inicializar("loose");
     });
   }
 }
 
-function empezarJuego() {
+function startGame() {
   window.juego = new Juego();
 }
